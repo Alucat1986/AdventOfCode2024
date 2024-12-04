a5d1c5bfd429f6420ddc1ad7c0bee8fb600a9293
Alunya <tobias@hazardy.de>
2024-12-04 14:10:06 +0100

    updated Readme and Build file

--- Build.lua
+++ Build.lua
@@ -2,7 +2,7 @@
 workspace "AdventOfCode2024"
    architecture "x64"
    configurations { "Debug", "Release", "Dist" }
-   startproject "Day03"
+   startproject "Day05"
 
    -- Workspace-wide build options for MSVC
    filter "system:windows"
@@ -13,3 +13,5 @@ OutputDir = "%{cfg.system}-%{cfg.architecture}/%{cfg.buildcfg}"
 include "Day01/Build-Day01.lua"
 include "Day02/Build-Day02.lua"
 include "Day03/Build-Day03.lua"
+include "Day04/Build-Day04.lua"
+include "Day05/Build-Day05.lua"
--- README.md
+++ README.md
@@ -6,4 +6,5 @@ My next try on these Advent of Code Challenges for the Year 2024!
 Day #1: Complete
 Day #2: Only Part 1 works
 Day #3: Complete
-Day #4: Soon
+Day #4: Complete
+Day #5: Soon
