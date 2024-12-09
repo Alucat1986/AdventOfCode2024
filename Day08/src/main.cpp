a457620c54d5c5fcaa08b082c7cb871a3cc10375
Alunya <tobias@hazardy.de>
2024-12-09 02:08:05 +0100

    Finished Day 8.

--- Day08/src/main.cpp
+++ Day08/src/main.cpp
@@ -1,16 +1,45 @@
+#include <algorithm>
+#include <chrono>
 #include <fstream>
 #include <iostream>
+#include <iterator>
+#include <ranges>
 #include <string>
 #include <vector>
 
+// Technically a Node again but for readability
+struct Vector2D {
+	size_t x = 0;
+	size_t y = 0;
+};
+
+struct Node {
+	size_t PosX = 0;
+	size_t PosY = 0;
+
+	Node operator+(const Vector2D& other) const {
+		return Node{ PosX + other.x, PosY + other.y };
+	}
+	bool operator==(const Node& other) const = default;
+};
+
+struct Radar {
+	Node Location = { 0, 0 };
+	char Frequency = ',';
+};
+
 bool readFile(const std::string& filePath, std::vector<std::vector<char>>& data);
-unsigned int countXmas(const std::vector<std::vector<char>>& data);
-unsigned int countMas(const std::vector<std::vector<char>>& data);
+bool isInsideMap(const Node& node, const std::vector<std::vector<char>>& data);
+int countUniqueAntiNodeLocations(const std::vector<std::vector<char>>& data);
+int countUniqueAntiNodeLocationsPartII(const std::vector<std::vector<char>>& data);
+std::vector<Radar> findAllRadarDishes(const std::vector<std::vector<char>>& data);
+std::pair<Vector2D, Vector2D> calculateVector(Node first, Node second);
 
 int main() {
 	char fileToLoad;
 	std::string file = "input\\";
-	std::vector<std::vector<char>> xmasSearch;
+	std::vector<std::vector<char>> radarMap;
+	int locationsPI = 0, locationsPII = 0;
 
 	std::cout << "Loading (E)xample or (I)nput? ";
 	fileToLoad = std::cin.get();
@@ -22,177 +51,176 @@ int main() {
 	else
 		std::cout << "No File found with that name\n";
 
-	if ( !readFile(file, xmasSearch) )
+	if ( !readFile(file, radarMap) )
 		std::cout << "Couldn't open file \"" << file << "\"!\n";
 
-	std::cout << "XMAS Amount: " << countXmas(xmasSearch) << "\n";
-	std::cout << "MAS Amount: " << countMas(xmasSearch) << "\n";
+	const auto start = std::chrono::system_clock::now();
+	locationsPI = countUniqueAntiNodeLocations(radarMap);
+	const auto end = std::chrono::system_clock::now();
+
+	const auto startPII = std::chrono::system_clock::now();
+	locationsPII = countUniqueAntiNodeLocationsPartII(radarMap);
+	const auto endPII = std::chrono::system_clock::now();
+
+	std::cout << "Unique Locations of Antinodes: " << locationsPI << " : " << locationsPII << "\n";
+	std::cout << "Time of execution(P1): " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start) << "\n";
+	std::cout << "Time of execution(P2): " << std::chrono::duration_cast<std::chrono::milliseconds>(endPII - startPII) << "\n";
 
 	std::cin.get();
 }
 
 bool readFile(const std::string& filePath, std::vector<std::vector<char>>& data) {
 	std::ifstream fileToRead(filePath);
-	std::vector<char> lineVector;
 	std::string line;
-	size_t row = 0;
 
 	if ( !fileToRead.is_open() ) {
 		std::cout << "Failed to open " << filePath << " ...\n";
 		return false;
-	}
+	} // if ( !fileToRead.is_open() )
 
 	std::cout << "Reading file " << filePath << " ...\n";
 	while ( std::getline(fileToRead, line) ) {
+		std::vector<char> lineVector;
+
+#ifdef DEBUG
 		std::cout << "Length: " << line.size() << "\t";
 		std::cout << "Line: " << line;
+		std::cout << "\t\t|\t";
+#endif
+
+		for ( int pos = 0; pos < line.size(); pos++ ) {
+			lineVector.emplace_back(line[pos]);
+		} // for ( int pos = 0; pos < line.size(); pos++ )
+
+		data.push_back(lineVector);
+
+#ifdef DEBUG
+		std::cout << "Values:\t\t";
+		for ( const auto& element : lineVector ) {
+			std::cout << element << " ";
+		} // for ( const auto& element : lineVector )
 		std::cout << "\n";
-
-		for ( size_t pos = 0; pos < line.size(); pos++ ) {
-			lineVector.emplace_back(line.at(pos));
-		}
-
-		data.emplace_back(lineVector);
-		lineVector.clear();
-		row++;
+#endif
 	} // while ( std::getline(fileToRead, line) )
 
 	fileToRead.close();
+	std::cout << "Finished reading file " << filePath << " ...\n";
 	return true;
 }
 
-unsigned int countXmas(const std::vector<std::vector<char>>& data) {
-	unsigned int result = 0;
-
-	for ( size_t row = 0; row < data.size(); row++ ) {
-		for ( size_t column = 0; column < data.at(row).size(); column++ ) {
-			if ( data.at(row).at(column) != 'X' )
-				continue;
-
-			// You can check upwards if the row is low enough
-			if ( row >= 3 ) {
-				if ( data.at(row - 1).at(column) == 'M' &&
-					data.at(row - 2).at(column) == 'A' &&
-					data.at(row - 3).at(column) == 'S' )
-					result++;
-
-				if ( column >= 3 ) {
-					if ( data.at(row - 1).at(column - 1) == 'M' &&
-						data.at(row - 2).at(column - 2) == 'A' &&
-						data.at(row - 3).at(column - 3) == 'S' )
-						result++;
-				}
-
-				if ( column <= data.at(row).size() - 4 ) {
-					if ( data.at(row - 1).at(column + 1) == 'M' &&
-						data.at(row - 2).at(column + 2) == 'A' &&
-						data.at(row - 3).at(column + 3) == 'S' )
-						result++;
-				}
-			}
-
-			// You can check downwards if the row is not too low
-			if ( row <= data.size() - 4 ) {
-				if ( data.at(row + 1).at(column) == 'M' &&
-					data.at(row + 2).at(column) == 'A' &&
-					data.at(row + 3).at(column) == 'S' )
-					result++;
-				if ( column >= 3 ) {
-					if ( data.at(row + 1).at(column - 1) == 'M' &&
-						data.at(row + 2).at(column - 2) == 'A' &&
-						data.at(row + 3).at(column - 3) == 'S' )
-						result++;
-				}
-
-				if ( column <= data.at(row).size() - 4 ) {
-					if ( data.at(row + 1).at(column + 1) == 'M' &&
-						data.at(row + 2).at(column + 2) == 'A' &&
-						data.at(row + 3).at(column + 3) == 'S' )
-						result++;
-				}
-			}
-
-			// You can check to the right if the column is not to close to the right edge
-			if ( column <= data.at(row).size() - 4 ) {
-				if ( data.at(row).at(column + 1) == 'M' &&
-					data.at(row).at(column + 2) == 'A' &&
-					data.at(row).at(column + 3) == 'S' )
-					result++;
-			}
-
-			// You can check to the left if the column is not to close to the left edge
-			if ( column >= 3 ) {
-				if ( data.at(row).at(column - 1) == 'M' &&
-					data.at(row).at(column - 2) == 'A' &&
-					data.at(row).at(column - 3) == 'S' )
-					result++;
-			}
-		}
-	}
-	return result;
+bool isInsideMap(const Node& node, const std::vector<std::vector<char>>& data) {
+	return node.PosX >= 0 && node.PosX < data[0].size() &&
+		   node.PosY >= 0 && node.PosY < data.size();
 }
 
-unsigned int countMas(const std::vector<std::vector<char>>& data) {
-	unsigned int result = 0;
+int countUniqueAntiNodeLocations(const std::vector<std::vector<char>>& data) {
+	std::vector<Node> visitedNodes;
+	std::vector<Radar> radarDishes = findAllRadarDishes(data);
 
-	for ( size_t row = 0; row < data.size(); row++ ) {
-		for ( size_t column = 0; column < data.at(row).size(); column++ ) {
-			if ( data.at(row).at(column) != 'A' )
-				continue;
+	for ( auto currentDishIter = radarDishes.begin(); currentDishIter != radarDishes.end(); currentDishIter++ ) {
+		auto pairUpIter = currentDishIter + 1;
+		while ( pairUpIter != radarDishes.end() ) {
+			if ( currentDishIter->Frequency == pairUpIter->Frequency ) {
+				std::pair<Vector2D, Vector2D> distances = calculateVector(Node{ currentDishIter->Location }, Node{ pairUpIter->Location });
+				Node antiNodeA = currentDishIter->Location + std::get<1>(distances);
+				Node antiNodeB = pairUpIter->Location + std::get<0>(distances);
 
-			if ( row >= 1 && column >= 1 && row < data.size() - 1 && column < data.at(row).size() - 1 ) {
-				/*
-				// Checking for + shapes
-				if ( data.at(row - 1).at(column) == 'M' &&
-					data.at(row).at(column - 1) == 'M' &&
-					data.at(row + 1).at(column) == 'S' &&
-					data.at(row).at(column + 1) == 'S' )
-					result++;
+				if ( isInsideMap(antiNodeA, data) && !std::ranges::contains(visitedNodes, antiNodeA) ) {
+#ifdef DEBUG
+					std::cout << "A(" << antiNodeA.PosX << "/" << antiNodeA.PosY << ")\n";
+#endif
+					visitedNodes.push_back(antiNodeA);
+				} // if ( isInsideMap(antiNodeA, data) )
+				if ( isInsideMap(antiNodeB, data) && !std::ranges::contains(visitedNodes, antiNodeB) ) {
+#ifdef DEBUG
+					std::cout << "B(" << antiNodeB.PosX << "/" << antiNodeB.PosY << ")\n";
+#endif
+					visitedNodes.push_back(antiNodeB);
+				} // if ( isInsideMap(antiNodeB, data) )
+			} // if ( currentDishIter->Frequency == pairUpIter->Frequency )
+			pairUpIter++;
+		} // while ( pairUpIter != radarDishes.end() )
+	} // for ( auto& currentDishIter = radarDishes.begin(); currentDishIter != radarDishes.end(); currentDishIter++ )
+	return static_cast<int>(visitedNodes.size());
+}
+
+// Part 2
+int countUniqueAntiNodeLocationsPartII(const std::vector<std::vector<char>>& data) {
+	std::vector<Node> visitedNodes;
+	std::vector<Radar> radarDishes = findAllRadarDishes(data);
+
+	for ( auto currentDishIter = radarDishes.begin(); currentDishIter != radarDishes.end(); currentDishIter++ ) {
+		auto pairUpIter = currentDishIter + 1;
+		while ( pairUpIter != radarDishes.end() ) {
+			if ( currentDishIter->Frequency == pairUpIter->Frequency ) {
+				std::pair<Vector2D, Vector2D> distances = calculateVector(Node{ currentDishIter->Location }, Node{ pairUpIter->Location });
+				Node antiNodeA = currentDishIter->Location + std::get<1>(distances);
+				Node antiNodeB = pairUpIter->Location + std::get<0>(distances);
+
+				if ( !std::ranges::contains(visitedNodes, currentDishIter->Location) ) {
+					visitedNodes.push_back(currentDishIter->Location);
+#ifdef DEBUG
+					std::cout << "C(" << currentDishIter->Location.PosX << "/" << currentDishIter->Location.PosY << ")\n";
+#endif
+				}
+					
+				if ( !std::ranges::contains(visitedNodes, pairUpIter->Location) ) {
+					visitedNodes.push_back(pairUpIter->Location);
+#ifdef DEBUG
+					std::cout << "P(" << pairUpIter->Location.PosX << "/" << pairUpIter->Location.PosY << ")\n";
+#endif
+				}
+					
+
+				while ( isInsideMap(antiNodeA, data) ) {
+					if ( !std::ranges::contains(visitedNodes, antiNodeA) ) {
+#ifdef DEBUG
+						std::cout << "A(" << antiNodeA.PosX << "/" << antiNodeA.PosY << ")\n";
+#endif
+						visitedNodes.push_back(antiNodeA);
+					} // if ( isInsideMap(antiNodeA, data) )
+					antiNodeA = antiNodeA + std::get<1>(distances);
+				} // while ( isInsideMap(antiNodeA, data) )
 				
-				if ( data.at(row - 1).at(column) == 'S' &&
-					data.at(row).at(column - 1) == 'S' &&
-					data.at(row + 1).at(column) == 'M' &&
-					data.at(row).at(column + 1) == 'M' )
-					result++;
+				while ( isInsideMap(antiNodeB, data) ) {
+					if ( !std::ranges::contains(visitedNodes, antiNodeB) ) {
+#ifdef DEBUG
+						std::cout << "B(" << antiNodeB.PosX << "/" << antiNodeB.PosY << ")\n";
+#endif
+						visitedNodes.push_back(antiNodeB);
+					} // if ( isInsideMap(antiNodeB, data) )
+					antiNodeB = antiNodeB + std::get<0>(distances);
+				} // while ( isInsideMap(antiNodeB, data) )
+			} // if ( *currentDishIter == *pairUpIter )
+			pairUpIter++;
+		} // while ( pairUpIter != radarDishes.end() )
+	} // for ( auto& currentDishIter = radarDishes.begin(); currentDishIter != radarDishes.end(); currentDishIter++ )
+	return static_cast<int>(visitedNodes.size());
+}
 
-				if ( data.at(row - 1).at(column) == 'M' &&
-					data.at(row).at(column - 1) == 'S' &&
-					data.at(row + 1).at(column) == 'S' &&
-					data.at(row).at(column + 1) == 'M' )
-					result++;
+std::vector<Radar> findAllRadarDishes(const std::vector<std::vector<char>>& data) {
+	std::vector<Radar> radarDishes;
 
-				if ( data.at(row - 1).at(column) == 'S' &&
-					data.at(row).at(column - 1) == 'M' &&
-					data.at(row + 1).at(column) == 'M' &&
-					data.at(row).at(column + 1) == 'S' )
-					result++;
-				*/
-				// Checking for x shapes
-				if ( data.at(row - 1).at(column - 1) == 'M' &&
-					data.at(row - 1).at(column + 1) == 'M' &&
-					data.at(row + 1).at(column - 1) == 'S' &&
-					data.at(row + 1).at(column + 1) == 'S' )
-					result++;
+	auto notDot = [](auto columnAndElement) { return std::get<1>(columnAndElement) != '.'; };
+#ifdef DEBUG
+	std::cout << "\nFinding all radar dishes and their frequencies...\n";
+#endif
+	for ( size_t row = 0; row < data.size(); row++ ) {
+		for ( const auto& [column, symbol] : std::views::enumerate(data[row]) | std::views::filter(notDot) ) {
+			radarDishes.emplace_back(Radar{ Node{static_cast<size_t>(column), row}, symbol });
+#ifdef DEBUG
+			std::cout << "Radar(" << column << "/" << row << "|" << symbol << ")\n";
+#endif
+		} // for ( const auto& [column, symbol] : std::views::enumerate(data[row]) | std::views::filter(notDot) )
+	} // for ( size_t row = 0; row < data.size(); row++ )
+#ifdef DEBUG
+	std::cout << "All found.\n";
+#endif
+	return radarDishes;
+}
 
-				if ( data.at(row - 1).at(column - 1) == 'S' &&
-					data.at(row - 1).at(column + 1) == 'S' &&
-					data.at(row + 1).at(column - 1) == 'M' &&
-					data.at(row + 1).at(column + 1) == 'M' )
-					result++;
-
-				if ( data.at(row - 1).at(column - 1) == 'M' &&
-					data.at(row - 1).at(column + 1) == 'S' &&
-					data.at(row + 1).at(column - 1) == 'M' &&
-					data.at(row + 1).at(column + 1) == 'S' )
-					result++;
-
-				if ( data.at(row - 1).at(column - 1) == 'S' &&
-					data.at(row - 1).at(column + 1) == 'M' &&
-					data.at(row + 1).at(column - 1) == 'S' &&
-					data.at(row + 1).at(column + 1) == 'M' )
-					result++;
-			}
-		}
-	}
-	return result;
+std::pair<Vector2D, Vector2D> calculateVector(Node first, Node second) {
+	Vector2D forward{ second.PosX - first.PosX, second.PosY - first.PosY };
+	Vector2D backward{ first.PosX - second.PosX, first.PosY - second.PosY };
+	return { forward, backward };
 }
--- README.md
+++ README.md
@@ -3,11 +3,26 @@
 My next try on these Advent of Code Challenges for the Year 2024! 
 
 ## List of Days and their status
-* Day #1: Complete
-* Day #2: Only Part 1 works
-* Day #3: Complete
-* Day #4: Complete
-* Day #5: Complete
-* Day #6: Complete, for P2 I recommened Release build or it might take roughly ~1hr
-* Day #7: Complete
-* Day #8: Soon
+* Day #01: Complete
+* Day #02: Only Part 1 works
+* Day #03: Complete
+* Day #04: Complete
+* Day #05: Complete
+* Day #06: Complete, for P2 I recommened Release build or it might take roughly ~1hr
+* Day #07: Complete
+* Day #08: Soon
+* Day #09: Soon
+* Day #10: Soon
+* Day #11: Soon
+* Day #12: Soon
+* Day #13: Soon
+* Day #14: Soon
+* Day #15: Soon
+* Day #16: Soon
+* Day #17: Soon
+* Day #18: Soon
+* Day #19: Soon
+* Day #20: Soon
+* Day #21: Soon
+* Day #22: Soon
+* Day #23: Soon
