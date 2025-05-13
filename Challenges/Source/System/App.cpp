a6b28270ea6591d3443b2ff54e9cf4fa00cadd62
Alunya <tobias@hazardy.de>
2025-05-13 22:53:03 +0200

    Added the implementation files of the menu and app classes.

---
+++ Challenges/Source/System/App.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file App.cpp
+ * @author Alunya
+ * @brief Contains the app class implementation.
+ * @date 13.05.2025
+ */
+
+#include "Include/System/App.hpp"
+
+#include "Include/UI/Menu.hpp"
+
+namespace aoc {
+
+App::App() {} // App::App()
+
+void App::Run() {} // void App::Run( ... )
+
+} // namespace aoc
---
+++ Challenges/Source/UI/Menu.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file Menu.hpp
+ * @author Alunya
+ * @brief Contains the menu class implementation.
+ * @date 13.05.2025
+ */
+
+#include "Include/UI/Menu.hpp"
+
+namespace aoc {
+
+Menu::Menu() {} // Menu::Menu( ... )
+
+} // namespace aoc
