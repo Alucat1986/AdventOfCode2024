e587ca6b59f1aa9edfb2b81413471a0ef02b8874
Alunya <tobias@hazardy.de>
2025-05-13 20:03:23 +0200

    Added ´App.hpp` and `Menu.hpp`.
    They will handle the main loop and menu accordingly.

---
+++ Challenges/Include/System/App.hpp
@@ -0,0 +1,37 @@
+/**
+ * @file App.hpp
+ * @author Alunya
+ * @brief Contains the App class declaration.
+ * @date 13.05.2025
+ */
+
+#ifndef APP_HPP
+#define APP_HPP
+
+#include "../UI/Menu.hpp"
+
+namespace aoc {
+/**
+ * @class App
+ * @brief Manages the main loop and state of the program.
+ * @author Alunya
+ * @date 13.05.2025
+ *
+ * The App class is responsible for managing the main loop and state of
+ * the program. It initializes the menu, starts the corresponding challenges
+ * and times them.
+ */
+class App {
+public:
+    App();
+
+private:
+    void Run();
+
+private:
+    Menu mMenu;
+};
+
+} // namespace aoc
+
+#endif // ifndef APP_HPP
---
+++ Challenges/Include/UI/Menu.hpp
@@ -0,0 +1,25 @@
+/**
+ * @file Menu.hpp
+ * @author Alunya
+ * @brief Contains the Menu class declaration.
+ * @date 13.05.2025
+ */
+
+#ifndef MENU_HPP
+#define MENU_HPP
+
+namespace aoc {
+/**
+ * @class Menu
+ * @brief Manages the console menu and user input.
+ * @author Alunya
+ * @date 13.05.2025
+ */
+class Menu {
+public:
+    Menu();
+}; // class Menu
+
+} // namespace aoc
+
+#endif // ifndef MENU_HPP
